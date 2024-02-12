43f99e6866b84c185d558f2a5814e1db8f88eb40
Nicolas <Nicolas@DESKTOP-7A4U4C3.home>
2024-02-12 18:00:29 +0100

    JavaFX changes #2 Finish

--- src/test/java/app/MonApplication.java
+++ src/test/java/app/MonApplication.java
@@ -1,14 +1,31 @@
 package app;
 
-public class MonApplication {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MonApplication extends Application{
     
     public static void main(String[] args) {
+    	
+    	launch(args);
+    	
     	FichierCSV csv = new FichierCSV();
     	GestionnaireFinance GeFi = new GestionnaireFinance();
 		MonApplication.affichageCSV(csv, GeFi);
 		MonApplication.affichageTransaction(GeFi);
-		
 	}
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, JavaFX!");
+        Scene scene = new Scene(label, 400, 300);
+        
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
     private static void affichageCSV(FichierCSV csv, GestionnaireFinance GeFi) {
 		csv.chargerDonnees();
