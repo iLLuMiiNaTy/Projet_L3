cc73c1aabe2428864f671e045f8aff74d3593c53
EmmaMinguez <EmmaMinguez@Emma.private.univ-tlse3.fr>
2024-02-12 16:44:43 +0100

    code GestionnaireProduction

--- src/test/java/app/GestionnaireProduction.java
+++ src/test/java/app/GestionnaireProduction.java
@@ -1,5 +1,36 @@
 package app;
 
+import java.util.ArrayList;
+
+
+
 public class GestionnaireProduction {
 
+private ArrayList <ChaineDeProduction>chaine;
+	
+	public GestionnaireProduction(){
+		chaine = new ArrayList<>();
+	}
+	
+	public void ajouterChaine(ChaineDeProduction c) {
+		chaine.add(c);
+	}
+	
+
+	public void ajusterNiveauActivation(String codeChaine, int niveauActivation) {
+		boolean chaineOK = false;
+		for (ChaineDeProduction c: chaine) {
+			if (c.getCode().equals(codeChaine)) {
+				c.setNiveauActivation(niveauActivation);
+				chaineOK = true;
+				c.activer(niveauActivation);
+			}
+		}
+		if (chaineOK = false) {
+			System.out.println("Aucune chaîne de production trouvée avec le code : " + codeChaine);
+		}
+		
+	}
+	
+	//méthode SimulerProduction
 }
